1945446dbcdc60b9941e721c386fd2f657b8592f
Luke Fowler <luciusfow@gmail.com>
2021-11-30 21:20:24 -0500

    commit tree 5bb7c20931f66799fc706941de5ed0c8c59a14e8

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, same){
+return drivers.filter(function(x){
+    return x.toLowerCase() === same.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, firstLetter){
+    return drivers.filter(function(x){
+    return x.toLowerCase().indexOf(firstLetter.toLowerCase()) === 0
+    })
+}
+
+function matchName(drivers, possMatch){
+    return drivers.filter(function(x){
+        return x.name === possMatch
+    })
+
+}
